d8686baa5d3d95aa179ed5b212de4541f8987fd1
Psyanic <62394630+Psyanic@users.noreply.github.com>
2021-02-24 13:33:36 +0000

    Add, Edit and Assign functions
    Base functions for add, edit and assign without any database interaction

---
+++ KeepItTogether_AddEditAssign/add.java
@@ -0,0 +1,10 @@
+
+public class add {
+	public void chore(int choreId, String desc, boolean repeat, int houseId, boolean complete, int userId) {
+		//add to database
+	}
+	
+	public void bill(int trnsId, String desc, boolean repeat, int prodId, int houseId, float price, boolean complete, int userId) {
+		//add to database
+	}
+}
---
+++ KeepItTogether_AddEditAssign/assign.java
@@ -0,0 +1,10 @@
+
+public class assign {
+	public void chore(int choreId, int houseId, int userId) {
+		//add to database
+	}
+	
+	public void bill(int trnsId, int houseId, int userId) {
+		//add to database
+	}
+}
