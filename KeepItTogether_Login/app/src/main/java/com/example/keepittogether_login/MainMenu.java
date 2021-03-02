b9fa850ac047f079076ee9d9c928e916a93bb107
ariel <ak2774@bath.ac.uk>
2021-03-02 18:31:11 +0000

    Added a view Page

--- KeepItTogether_Login/app/src/main/java/com/example/keepittogether_login/MainMenu.java
+++ KeepItTogether_Login/app/src/main/java/com/example/keepittogether_login/MainMenu.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,13 @@ public class MainMenu extends AppCompatActivity {
         btView = findViewById(R.id.bt_view);
         btAdd = findViewById(R.id.bt_add);
         btYour_tasks = findViewById(R.id.bt_your_tasks);
+
+        btView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this , com.example.keepittogether_login.View.class));
+                }
+        });
 
     }
 }
