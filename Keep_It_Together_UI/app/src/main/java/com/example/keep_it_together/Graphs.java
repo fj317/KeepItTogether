0cc690a4b92f19af0559928f912e1f3ee7a494c9
Freddie Jonas <fj6787@gmail.com>
2021-03-15 19:13:01 +0000

    Updated graphs

--- Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Graphs.java
+++ Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Graphs.java
@@ -2,13 +2,36 @@ package com.example.keep_it_together;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class Graphs extends AppCompatActivity {
+    int day, month, year, hour, minute;
+    Button dateSelector;
 
+    // https://www.truiton.com/2015/04/android-date-range-picker/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
+        dateSelector = findViewById(R.id.bt_date_selector);
+
+        dateSelector.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                Calendar calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+               // DatePickerDialog datePickerDialog = new DatePickerDialog(Graphs.this, Graphs.this, year, month, day);
+                //datePickerDialog.show();
+            }
+        });
+
     }
 }
--- Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/View.java
+++ Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/View.java
@@ -7,18 +7,27 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class View extends AppCompatActivity {
-    Button btCompletedTasks;
+    Button btCompletedTasks, btGraphs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         btCompletedTasks = findViewById(R.id.bt_completed);
+        btGraphs = findViewById(R.id.bt_graphs);
+
 
         btCompletedTasks.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(android.view.View v) {
                 startActivity(new Intent(View.this , com.example.keep_it_together.CompletedTasks.class));
+            }
+        });
+
+        btGraphs.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                startActivity(new Intent(View.this , com.example.keep_it_together.Graphs.class));
             }
         });
 
