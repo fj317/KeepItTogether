32a956f0d66c432be78c5958dc313220610f5b55
Freddie Jonas <fj6787@gmail.com>
2021-03-16 12:17:09 +0000

    Included slider to select days

--- Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Graphs.java
+++ Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Graphs.java
@@ -6,32 +6,46 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class Graphs extends AppCompatActivity {
-    int day, month, year, hour, minute;
-    Button dateSelector;
+    int days;
+    SeekBar seekBar;
+    TextView textDays;
+    // reference https://stackoverflow.com/questions/8629535/implementing-a-slider-seekbar-in-android
 
-    // https://www.truiton.com/2015/04/android-date-range-picker/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
-        dateSelector = findViewById(R.id.bt_date_selector);
-
-        dateSelector.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                Calendar calendar = Calendar.getInstance();
-                year = calendar.get(Calendar.YEAR);
-                month = calendar.get(Calendar.MONTH);
-                day = calendar.get(Calendar.DAY_OF_MONTH);
-               // DatePickerDialog datePickerDialog = new DatePickerDialog(Graphs.this, Graphs.this, year, month, day);
-                //datePickerDialog.show();
-            }
-        });
-
+        // set a change listener on the SeekBar
+        seekBar = findViewById(R.id.seekBarDays);
+        textDays = findViewById(R.id.textDays);
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
     }
+
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            // updated continuously as the user slides the thumb
+            textDays.setText(String.valueOf(progress));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // called when the user first touches the SeekBar
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // called after the user finishes moving the SeekBar
+            days = seekBar.getProgress();
+            // call graph making bit here
+        }
+    };
 }
