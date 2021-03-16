912b4709147222b0a2228287c5fa81dff926ca92
Fabiana <78922552+fabianarotaru@users.noreply.github.com>
2021-03-16 16:59:56 +0000

    Update Household.java

--- Household.java
+++ Household.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Household {
 
@@ -7,30 +8,47 @@ public class Household {
         Client dbConnection = new Client("86.9.93.210", 58934);
         String[] find = dbConnection.select("SELECT house_id FROM House WHERE address = '" + address + "' AND postcode = '" + postcode + "'");
         if (find[0].isEmpty()) {
-            dbConnection.modify("INSERT INTO House (address, postcode, number_of_residents) VALUES ('" + address + "', '" + postcode + "', " + numberOfResidents + ")");
-            System.out.println("test");
+            String houseID = randomIdGenerator();
+            dbConnection.modify("INSERT INTO House (house_id, address, postcode, number_of_residents) VALUES ('" + houseID + "', '" + address + "', '" + postcode + "', " + numberOfResidents + ")");
         }
         else {
             System.out.println("This household has already been created.");
         }
     }
 
-    public void join(int houseID, int userID) throws IOException {
+    public void join(String houseID, int userID) throws IOException {
         Client dbConnection = new Client("86.9.93.210", 58934);
-        String[] find = dbConnection.select("SELECT house_id FROM House WHERE house_id = " + houseID);
+        String[] find = dbConnection.select("SELECT house_id FROM House WHERE house_id = '" + houseID + "'");
         if (find[0].isEmpty()) {
             System.out.println("This household does not exist.");
         }
         else {
-            dbConnection.modify("UPDATE House SET number_of_residents = number_of_residents + 1 WHERE house_id = " + houseID);
-            dbConnection.modify("INSERT INTO HouseUsers (user_id, house_id) VALUES (" + userID + ", " + houseID + ")");
+            dbConnection.modify("UPDATE House SET number_of_residents = number_of_residents + 1 WHERE house_id = '" + houseID + "'");
+            dbConnection.modify("INSERT INTO HouseUsers (user_id, house_id) VALUES (" + userID + ", '" + houseID + "')");
         }
     }
 
-    public void leave(int houseID, int userID) throws IOException {
+    public void leave(String houseID, int userID) throws IOException {
         Client dbConnection = new Client("86.9.93.210", 58934);
-        dbConnection.modify("UPDATE House SET number_of_residents = number_of_residents - 1 WHERE house_id = " + houseID);
+        dbConnection.modify("UPDATE House SET number_of_residents = number_of_residents - 1 WHERE house_id = '" + houseID + "'");
         dbConnection.modify("DELETE FROM HouseUsers WHERE user_id = " + userID);
+    }
+    
+    public String randomIdGenerator() throws IOException {
+        Client dbConnection = new Client("86.9.93.210", 58934);
+        char[] chars = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
+        Random rand  = new Random();
+        int length = 10;
+        StringBuilder builder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            builder.append(chars[rand.nextInt(36)]);
+        }
+        String randomID = builder.toString();
+        String[] checkUniqueness = dbConnection.select("SELECT house_id FROM House WHERE house_id = '" + randomID + "'");
+        if (!checkUniqueness[0].isEmpty()) {
+            randomIdGenerator();
+        }
+        return randomID;
     }
 
     public static void main(String[] args) throws IOException {
