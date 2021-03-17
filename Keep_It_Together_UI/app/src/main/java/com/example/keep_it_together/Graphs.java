dc307ec8f44e2cef304e4d9868d77bc5dcaeade3
Freddie Jonas <fj6787@gmail.com>
2021-03-17 14:14:43 +0000

    Setting up graph page with required objects

--- Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Graphs.java
+++ Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Graphs.java
@@ -1,21 +1,27 @@
 package com.example.keep_it_together;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Graphs extends AppCompatActivity {
     int days;
     SeekBar seekBar;
     TextView textDays;
+    Switch graphTypeSwitch;
     // reference https://stackoverflow.com/questions/8629535/implementing-a-slider-seekbar-in-android
 
     @Override
@@ -25,6 +31,10 @@ public class Graphs extends AppCompatActivity {
         // set a change listener on the SeekBar
         seekBar = findViewById(R.id.seekBarDays);
         textDays = findViewById(R.id.textDays);
+        graphTypeSwitch = findViewById(R.id.chore_transaction_switch);
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        String houseID = preferences.getString("houseID", "");
+
         seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
     }
 
@@ -45,7 +55,17 @@ public class Graphs extends AppCompatActivity {
         public void onStopTrackingTouch(SeekBar seekBar) {
             // called after the user finishes moving the SeekBar
             days = seekBar.getProgress();
-            // call graph making bit here
+            if (graphTypeSwitch.isChecked()) {
+                // call transaction making graph code
+            } else {
+                // call chore making graph code
+            }
+
         }
     };
+
+    private void drawGraph(Dictionary data) {
+
+    }
+
 }
