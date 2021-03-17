af1289e2261d1f372b20399c99ef1db8a6522386
Freddie Jonas <fj6787@gmail.com>
2021-03-17 13:06:02 +0000

    Added progress bar

--- Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Register.java
+++ Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/Register.java
@@ -13,12 +13,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
 public class Register extends AppCompatActivity {
     EditText etName, etEmail, etPassword, etRepeatPassword;
     Button btSubmit;
+    ProgressBar progressBar;
     Client dbConnection = null;
 
     @Override
@@ -31,12 +33,15 @@ public class Register extends AppCompatActivity {
         etPassword = findViewById(R.id.et_password);
         etRepeatPassword = findViewById(R.id.et_Re_password);
         btSubmit = findViewById(R.id.bt_submit);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
         btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                         .permitAll().build();
                 StrictMode.setThreadPolicy(policy);
+                progressBar.setVisibility(View.VISIBLE);
                 AsyncTaskRunner runner = new AsyncTaskRunner();
                 runner.execute();
             }
@@ -47,7 +52,7 @@ public class Register extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             try {
-                dbConnection = new Client("86.8.35.12", 58934);
+                dbConnection = new Client("86.9.93.210", 58934);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -74,12 +79,14 @@ public class Register extends AppCompatActivity {
             if (name.equals("") || email.equals("") || password.equals("") || repeatPassword.equals("")) {
                 // send error message
                 Toast.makeText(getApplicationContext(), "No data in text-box field.",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
                 return;
             }
             // if passwords dont match
             if (!password.equals(repeatPassword)) {
                 // send error message
                 Toast.makeText(getApplicationContext(), "Passwords don't match!",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
                 return;
             }
             // check email is valid
@@ -87,6 +94,7 @@ public class Register extends AppCompatActivity {
             if (!(matcher.matches())) {
                 // error message
                 Toast.makeText(getApplicationContext(), "Email is not valid",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
                 return;
             }
             // check password is valid (at least 8 characters, must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number, can contain special character)
@@ -94,6 +102,7 @@ public class Register extends AppCompatActivity {
             if (!(matcher.matches())) {
                 // error message
                 Toast.makeText(getApplicationContext(), "Password is not valid. It must be: 8 characters long, and contain at least 1 uppercase, 1 lowercase and 1 number.",Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
                 return;
             }
             String dbRequest = "SELECT email FROM users WHERE email = '" + email + "'";
@@ -104,9 +113,10 @@ public class Register extends AppCompatActivity {
                 e.printStackTrace();
             }
             // check if email is already in database
-            if (!dbResponse[0].isEmpty()) {
+            if (!(dbResponse[0] == null)) {
                 // error message
                 Toast.makeText(getApplicationContext(), "Email already in use.",Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
                 return;
             }
 
@@ -119,6 +129,8 @@ public class Register extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Register successful!",Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(getApplicationContext(), "Register unsuccessful, please try again.",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+                return;
             }
             // go back to login page now registering is done
             startActivity(new Intent(Register.this , AccountLogin.class));
