7b9a57d3d8b7f543f92a8fb4286c5ec666dd4445
Freddie Jonas <fj6787@gmail.com>
2021-03-17 13:06:24 +0000

    Added logic to check if user is in a house or not

--- Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/MainMenu.java
+++ Keep_It_Together_UI/app/src/main/java/com/example/keep_it_together/MainMenu.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 public class MainMenu extends AppCompatActivity {
     Button btYourTasks, btView, btAdd, btYourHouse;
 
@@ -20,9 +22,6 @@ public class MainMenu extends AppCompatActivity {
         btAdd = findViewById(R.id.bt_add);
         btYourTasks = findViewById(R.id.bt_your_tasks);
         btYourHouse = findViewById(R.id.bt_your_house);
-
-        //SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
-        //btYourHouse.setText(preferences.getString("userID", ""));
 
         btYourTasks.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,8 +49,30 @@ public class MainMenu extends AppCompatActivity {
             public void onClick(View v) {
                 // work out if user is in a house or not
                 // work out which page to send user to (userNoHouse or yourHouse)
+                try {
+                    if (userInHouse()) {
+                        startActivity(new Intent(MainMenu.this , com.example.keep_it_together.YourHouse.class));
+                    } else {
+                        startActivity(new Intent(MainMenu.this , com.example.keep_it_together.UserNoHouse.class));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 startActivity(new Intent(MainMenu.this , com.example.keep_it_together.UserNoHouse.class));
             }
         });
+
+    }
+
+    private boolean userInHouse() throws IOException {
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        String userID = preferences.getString("userID", "");
+        Client dbConnection = new Client("86.9.93.210", 58934);
+        String dbRequest = "SELECT user_id, house_id FROM HouseUsers WHERE user_id = '" + userID + "'";
+        String[] dbResponse = dbConnection.select(dbRequest);
+        // if empty then no record exists so they arent part of any houses so return false
+        // otherwise return true
+        return !dbResponse[0].isEmpty();
     }
 }
