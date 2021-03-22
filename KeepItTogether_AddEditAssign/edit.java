ff89ca642b87900d7f4a3a8300acbc87be292dc8
Psyanic <62394630+Psyanic@users.noreply.github.com>
2021-03-22 10:34:28 +0000

    DB update
    Updated for the new database structure

--- KeepItTogether_AddEditAssign/edit.java
+++ KeepItTogether_AddEditAssign/edit.java
@@ -2,11 +2,12 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 public class edit {
-	public static void chore(String choreId, String desc, boolean complete, String userId) throws IOException {
+	public static void chore(String choreId, String desc, boolean complete, String userId, String name) throws IOException {
 		Client db = new Client("86.9.93.210", 58934);
 		LocalDate date = LocalDate.now();
 		if (complete) {
-			String chores = "UPDATE Chores SET description = '" + desc + "', last_completed = '" + date.toString() + "' ";
+			String chores = "UPDATE Chores SET description = '" + desc + "', last_completed = '" + date.toString() + "', ";
+			chores += "name = '" + name + "', active = 0 ";
 			chores += "WHERE chore_id = " + choreId;
 			db.modify(chores);
 			String choreRecords = "INSERT INTO ChoreRecords (user_id, chore_id, date_completed) VALUES (";
@@ -15,24 +16,18 @@ public class edit {
 			assign.chore(choreId, userId);
 		}
 		else {
-			String chores = "UPDATE chores SET description = '" + desc + "' WHERE chore_id = " + choreId;
+			String chores = "UPDATE chores SET description = '" + desc + ", active = 1, name = '" + name;
+			chores += "' WHERE chore_id = " + choreId;
 			db.modify(chores);
 			assign.chore(choreId, userId);
 		}
 	}
 	
-	public static void bill(String trnsId, String prodId, String houseId, String price, String userId, String date) throws IOException {
+	public static void bill(String trnsId, String houseId, String price, String userId, String date, String name, String desc) throws IOException {
 		Client db = new Client("86.9.93.210", 58934);
 		String trans = "UPDATE Transactions SET user_id = " + userId + ", house_id = '" + houseId;
-		trans += "', date = '" + date + "', product_id = " + prodId + ", price = " + price;
+		trans += "', date = '" + date + "', price = " + price + ", name = '" + name + "', description = '" + desc + "'";
 		trans += " WHERE transaction_id = " + trnsId;
 		db.modify(trans);
-	}
-	
-	public static void product(String prodId, String name, String desc) throws IOException {
-		Client db = new Client("86.9.93.210", 58934);
-		String prod = "UPDATE Products SET name = '" + name + "', description = '" + desc;
-		prod += "' WHERE product_id = " + prodId;
-		db.modify(prod);
 	}
 }
